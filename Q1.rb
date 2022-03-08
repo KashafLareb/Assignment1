39a738e1655628868fb58ed108c95e9a8e295364
Kashaf <kashaflareb@gmail.com>
2022-03-08 18:25:18 +0500

    Just Question 1 remaining

---
+++ Q1.rb
@@ -0,0 +1,32 @@
+# Input: ["ahffaksfajeeubsne", "jefaa"]
+# Output: aksfaje
+# Input: ["aaffhkksemckelloe", "fhea"]
+# Output: affhkkse
+str = "ahffaksfajeeubsne"
+sub_str1 = "jefaa" 
+str2 = "aaffhkksemckelloe"
+sub_str2 = "fhea"
+
+
+
+
+
+def compressWindow(start, last, sub_str, str)
+    no_of_iterations = last - start
+    new_array=[]
+    for x in new_array
+        new_array[i] = str[start+i]
+        puts str[i]
+    end
+    
+end
+
+count = 0
+size_of_sub_str1 = sub_str1.length
+match_indices=[]
+size_of_sub_str1.times do |i|
+    match_indices[i] = str.index(sub_str1[i])
+end
+
+compressWindow(match_indices.min,match_indices.max, sub_str1, str)
+
---
+++ Q2.rb
@@ -0,0 +1,41 @@
+# 1. The username is between 4 and 25 characters.
+# 2. It must start with a letter.
+# 3. It can only contain letters, numbers, and the underscore character.
+# 4. It cannot end with an underscore character.
+
+
+# Ensuring point 1
+def checkSize(str)
+    if str.length>3 && str.length<26
+        return true
+    else return false
+    end
+end
+
+def CodelandUsernameValidation(str)
+
+alphabet = /\A[a-zA-Z]/  # Ensuring point 2
+nonWordCharacter = /\W/  # Ensuring point 3
+
+if checkSize(str)
+    if str.start_with?(alphabet) && !(str.match?(nonWordCharacter)) && !(str.end_with?("_"))  # Ensuring point 4
+        return true
+    else
+        return false
+    end
+else
+    return false
+end  
+
+end
+
+
+def testingUsernameValidationFunction
+    puts "Enter the user name: "
+    inputString = gets
+    puts CodelandUsernameValidation(inputString.chomp)
+end
+
+testingUsernameValidationFunction
+
+
